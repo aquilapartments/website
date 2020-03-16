4a247aa327b864241548b0dff4ef52748a69d6f2
turinglabsorg <turinglabs@criptext.com>
2020-03-16 11:48:34 +0100

    updated install script

--- install.sh
+++ install.sh
@@ -4,3 +4,11 @@
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 npm install pm2 -g
+
+#INSTALL CERTBOT
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt install python-certbot-apache
+sudo ufw allow 'Apache Full'
+sudo ufw allow 3000
+
+echo "NOW RUN: sudo certbot --apache -d your_domain"
