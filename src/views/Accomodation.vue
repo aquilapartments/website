c60c247f073847a203e1c5a637013d4bc2a57d77
turinglabsorg <turinglabs@criptext.com>
2020-03-16 17:30:18 +0100

    about page

--- src/views/Accomodation.vue
+++ src/views/Accomodation.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <div class="row">
       <div class="col-sm-8 visible-xs">
-        <img src="../../public/placeholders/accomodation.jpg" style="width:100%">
+        <img :src="accomodation.main_image.replace('/public','')" style="width:100%">
       </div>
       <div class="col-sm-4">
         <div class="home-slide-description">
@@ -22,24 +22,9 @@
       <div class="row">
         <div class="col-sm-12">
           <carousel :perPage=2>
-            <slide>
+            <slide v-for="slide in slider" v-bind:key="slide.order">
               <div class="accomodation-slide">
-                <img src="../../public/placeholders/accomodation_2.jpg" style="width:100%">
-              </div>
-            </slide>
-            <slide>
-              <div class="accomodation-slide">
-                <img src="../../public/placeholders/accomodation_3.jpg" style="width:100%">
-              </div>
-            </slide>
-            <slide>
-              <div class="accomodation-slide">
-                <img src="../../public/placeholders/accomodation_2.jpg" style="width:100%">
-              </div>
-            </slide>
-            <slide>
-              <div class="accomodation-slide">
-                <img src="../../public/placeholders/accomodation_3.jpg" style="width:100%">
+                <div class="slidehome-img" :style="'height:300px; background-image:url(\'' + slide.image.replace('/public','') + '\')'"></div>
               </div>
             </slide>
           </carousel>
@@ -49,55 +34,55 @@
         <div class="col-sm-9">
           <div class="accomodation-description">
             <h1>{{ translations.description[language] }}<br><span style="font-weight:100">{{ accomodation.subtitle }}</span></h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dapibus ex velit, ac auctor nisi dictum luctus. Nullam ex tellus, interdum ac viverra eget, consectetur a metus. Vivamus urna dolor, rhoncus sit amet ante vitae, lobortis sodales orci. Nulla sodales eleifend dolor eget faucibus. Quisque nibh odio, dapibus at tempor venenatis, vestibulum at augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dapibus ex velit, ac auctor nisi dictum luctus. Nullam ex tellus, interdum ac viverra eget, consectetur a metus. Vivamus urna dolor, rhoncus sit amet ante vitae, lobortis sodales orci. Nulla sodales eleifend dolor eget faucibus. Quisque nibh odio, dapibus.</p>
+            <p>{{ accomodation.long_description }}</p>
           </div>
         </div>
         <div class="col-sm-3">
           <div class="accomodation-side-description">
-            <h3>Parking Garage</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus finibus augue, nec egestas ante. Nullam fringilla in elit sed posuere. Quisque gravida elit in felis consequat, ac dapibus felis maximus.</p>
+            <h3>{{ accomodation.right_title_1 }}</h3>
+            <p>{{ accomodation.right_description_1 }}</p>
 
-            <h3>Parking Garage</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus finibus augue, nec egestas ante. Nullam fringilla in elit sed posuere. Quisque gravida elit in felis consequat, ac dapibus felis maximus.</p>
+            <h3>{{ accomodation.right_title_2 }}</h3>
+            <p>{{ accomodation.right_description_2 }}</p>
 
-            <h3>Parking Garage</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus finibus augue, nec egestas ante. Nullam fringilla in elit sed posuere. Quisque gravida elit in felis consequat, ac dapibus felis maximus.</p>
+            <h3>{{ accomodation.right_title_3 }}</h3>
+            <p>{{ accomodation.right_description_3 }}</p>
 
-            <h3>Parking Garage</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus finibus augue, nec egestas ante. Nullam fringilla in elit sed posuere. Quisque gravida elit in felis consequat, ac dapibus felis maximus.</p>
+            <h3>{{ accomodation.right_title_4 }}</h3>
+            <p>{{ accomodation.right_description_4 }}</p>
           </div>
         </div>
       </div>
     </div><!-- container -->
 
     <div class="row">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3150.369585080524!2d15.282543015414682!3d37.85164187974546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x131411a39251c7f9%3A0x2654f47409ca387a!2sPiazza%20Var%C3%B2%2C%2098039%20Taormina%20ME!5e0!3m2!1sen!2sit!4v1580329882609!5m2!1sen!2sit" width="100%" height="550" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+      <iframe :src="accomodation.location" width="100%" height="550" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
     </div>
 
     <div class="">
       <div class="row" style="margin:150px 0; text-align:left;">
         <div class="col-sm-12 text-center" style="margin-bottom:50px;">
-          <h1><b>Book</b> <span style="font-weight:200!important">Now</span></h1>
+          <h1><b>{{ translations.book_now[language] }}</b></h1>
         </div>
         <div class="col-sm-4 offset-md-2">
-            <b-form-input v-model="form.from" class="aquila-input" placeholder="From: dd/mm/yyyy"></b-form-input>
-            <b-form-input v-model="form.to" class="aquila-input" placeholder="To: dd/mm/yyyy"></b-form-input>
-            <b-form-input v-model="form.name" class="aquila-input" placeholder="Your name"></b-form-input>
+            <b-form-input v-model="form.from" class="aquila-input" :placeholder="translations.from[language].replace('\'','').replace('\'','')"></b-form-input>
+            <b-form-input v-model="form.to" class="aquila-input" :placeholder="translations.to[language].replace('\'','').replace('\'','')"></b-form-input>
+            <b-form-input v-model="form.name" class="aquila-input" :placeholder="translations.name[language]"></b-form-input>
         </div>
         <div class="col-sm-4">
             <div class="row">
               <div class="col-sm-6">
-                <b-form-input v-model="form.adults" class="aquila-input" placeholder="Adults"></b-form-input>
+                <b-form-input v-model="form.adults" class="aquila-input" :placeholder="translations.adults[language]"></b-form-input>
               </div>
               <div class="col-sm-6">
-                <b-form-input v-model="form.children" class="aquila-input" placeholder="Children"></b-form-input>
+                <b-form-input v-model="form.children" class="aquila-input" :placeholder="translations.children[language]"></b-form-input>
               </div>
             </div>
-            <b-form-input v-model="form.message" class="aquila-input" placeholder="Message"></b-form-input>
-            <b-form-input v-model="form.email" type="email" class="aquila-input" placeholder="E-mail address"></b-form-input>
+            <b-form-input v-model="form.message" class="aquila-input" :placeholder="translations.message[language]"></b-form-input>
+            <b-form-input v-model="form.email" type="email" class="aquila-input" :placeholder="translations.email[language]"></b-form-input>
         </div>
         <div class="col-sm-12 text-center" style="margin-top:20px">
-          <div class="btn btn-aquila">BOOK NOW</div>
+          <div class="btn btn-aquila">{{ translations.book_now[language] }}</div>
         </div>
       </div>
     </div>
@@ -113,7 +98,10 @@
       return{
         translations: contents.translations,
         accomodations: contents.accomodation,
-        accomodation: '',
+        slider: contents.accomodationslider,
+        accomodation: {
+          main_image: ''
+        },
         language: 'en',
         form: {
           from: '',
